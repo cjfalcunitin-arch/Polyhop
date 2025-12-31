c5666eb1f365924dc7ae4d732588d32a176fd5a0
cjfalcunitin-arch <cjfalcunitin@gmail.com>
2025-12-31 13:31:44 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-import { showLoading } from "./Game/loading.js";
-import { showLobby } from "./Game/lobby.js";
-import { showGame } from "./Game/game.js";
-import { showTutorial } from "./Game/levels/tutorial.js";
-import { getCredit } from "./Game/system/credit.js";
-import { resetPickedCards } from "/Game/storage.js"; 
+import { showLoading } from "Game/loading.js";
+import { showLobby } from "Game/lobby.js";
+import { showGame } from "Game/game.js";
+import { showTutorial } from "Game/levels/tutorial.js";
+import { getCredit } from "Game/system/credit.js";
+import { resetPickedCards } from "Game/storage.js"; 
 import { markLevelCleared, isLevelUnlocked, isLevelCleared } from "/Game/system/progress.js";
 
 /* ===== GLOBAL FONT ===== */
